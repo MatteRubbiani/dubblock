26a5a301a5453c2ca6ba40c3d46bacd08df96f25
Jacopostortini <matterubbiani@gmail.com>
2020-04-08 19:54:16 +0200

    Iu

--- static/scripts/partita.js
+++ static/scripts/partita.js
@@ -1,4 +1,25 @@
+var interval;
+
 $(document).ready(()=>{
+    interval = setInterval(loadInfo, 100);
+
     new Player(0, 1, "players", 3, false, 0, 5, 2, 0).appendToListOfPlayers();
-    new Player(0, 2, "players", 3, false, 0, 5, 2, 0).appendToListOfPlayers();
 })
+
+function loadInfo(){
+    $.ajax({
+        url: BASE_URL+"get/"+localStorage.getItem("UserId"),
+        type: "get",
+        contentType: "application/json",
+        dataType: "json",
+        success: (data)=>{
+            /*
+            {
+                blocchi: [{corsia: int, id: int, livello: int}...{}],
+                griglia: {corsia: int, livelli: int, status: int},
+                users: [{corsia: null(se non sei tu), id: int, is_playing: boolean, livello: int, pedina_number: int, jolly_reveal: int, jolly_earthquake: int}...{}]
+            }
+            */
+        }
+    })
+}
